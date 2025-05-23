14cefbdd9228df414b48a76daec4f3e6652bcdfe
Pavlo Nikulin <pavlo.nikulin@obi.de>
2025-05-23 18:58:49 +0200

    add missing files

---
+++ nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,77 @@
+return {
+  'nvimdev/dashboard-nvim',
+  lazy = false,
+  opts = function()
+    local logo = {
+      '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
+      '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
+      '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
+      '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
+      '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
+      '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
+      '                                                  ',
+      '                                                  ',
+      '                                                  ',
+    }
+
+    local function center_padding(logo_height)
+      local win_height = vim.api.nvim_get_option 'lines' - 2 -- Window height minus 2 for borders
+      local padding = math.max(0, math.floor((win_height - logo_height) / 2))
+      return padding
+    end
+
+    local padding_lines = center_padding(#logo)
+
+    for _ = 1, padding_lines do
+      table.insert(logo, 1, '')
+    end
+
+    local opts = {
+      theme = 'doom',
+      hide = {
+        statusline = false,
+      },
+      config = {
+        header = logo,
+        center = {
+          {
+            icon = '  ',
+            desc = 'Find File                          ',
+            action = 'Telescope find_files',
+            key = 'ff',
+          },
+          {
+            icon = '  ',
+            desc = 'Find Word                          ',
+            action = 'Telescope live_grep',
+            key = 'fg',
+          },
+          {
+            icon = '  ',
+            desc = 'Refresh plugins                    ',
+            action = 'Lazy sync',
+            key = 'pr',
+          },
+        },
+        footer = {},
+      },
+    }
+
+    for _, button in ipairs(opts.config.center) do
+      button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+      button.key_format = '  %s'
+    end
+
+    if vim.o.filetype == 'lazy' then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'DashboardLoaded',
+        callback = function()
+          require('lazy').show()
+        end,
+      })
+    end
+
+    return opts
+  end,
+}
---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,22 @@
+return {
+  'iamcco/markdown-preview.nvim',
+
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+
+  build = 'cd app && yarn install',
+
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+
+  ft = { 'markdown' },
+
+  keys = {
+    {
+      '<leader>mp',
+      ft = 'markdown',
+      '<cmd>MarkdownPreviewToggle<cr>',
+      desc = 'Markdown Preview',
+    },
+  },
+}
